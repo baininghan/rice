8730bdc9fd0260c78ccbc80c8221e9fb4324cbe9
baininghan <lvsedehuanxiang@163.com>
2018-07-04 20:22:16 +0800

    add

---
+++ src/main/java/com/fancye/rice/_admin/demo/DemoController.java
@@ -0,0 +1,19 @@
+/**
+ * @Mailto: lvsedehuanxiang@163.com
+ * @Blog: baininghan.com
+ */
+package com.fancye.rice._admin.demo;
+
+import com.jfinal.core.Controller;
+
+/**
+ * @Description: layui示例
+ * @Author: Fancye Bai
+ * @Date: created in 19:10 2018/7/4
+ */
+public class DemoController extends Controller {
+
+    public void tab() {
+        render("tab.html");
+    }
+}
---
+++ src/main/java/com/fancye/rice/_admin/login/LoginController.java
@@ -0,0 +1,19 @@
+/**
+ * @Mailto: lvsedehuanxiang@163.com
+ * @Blog: baininghan.com
+ */
+package com.fancye.rice._admin.login;
+
+import com.jfinal.core.Controller;
+
+/**
+ * @Description:
+ * @Author: Fancye Bai
+ * @Date: created in 18:23 2018/7/3
+ */
+public class LoginController extends Controller {
+
+    public void index() {
+        render("index.html");
+    }
+}
